738bc499d56447328715b756c369c7e16a55641f
AkankshaMS <akanksha.makam@gmail.com>
2020-07-02 19:38:17 +0530

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image BlackScreen;
+
+    public GameObject blkimg;
+    public Animator anim;
+    public bool fadeout = true;
+
+    public bool fade = true;
+    public void changeScene(string scene_name){
+        blkimg.SetActive(true);       
+        Invoke("StartGame",1.0f);
+
+    }
+
+    public  void QuitGame () {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+         }
+    // Start is called before the first frame update
+    void Start()
+    {
+     fadeIn();
+    }
+void fadeIn(){
+        BlackScreen.canvasRenderer.SetAlpha(0.0f);
+        ToggleBlackscreen();
+}
+
+void ToggleBlackscreen(){
+        fade = !fade;
+
+        blkimg.SetActive(fade);
+    }
+void StartGame(){
+            Application.LoadLevel("Game");
+
+}
+
+}
